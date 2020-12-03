f65f3c75ab6dae7c8976c61923d5ec6271f747ce
emmikoivisto <emetsi@gmail.com>
2020-12-03 11:19:25 +0000

    can bring bookings by date

--- src/main/java/com/codeclan/example/CourseBookingsJavaSpring/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/CourseBookingsJavaSpring/controllers/BookingController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +19,13 @@ public class BookingController {
     BookingRepository bookingRepository;
 
     @GetMapping(value = "/bookings")
-    public ResponseEntity<List<Booking>> getAllBookings(){
+    public ResponseEntity<List<Booking>> getAllBookings(@RequestParam(name="date", required = false) String date){
+        if (date != null) {
+            return new ResponseEntity<>(bookingRepository.findByDate(date), HttpStatus.OK);
+        }
         return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
     }
+
+
 
 }
