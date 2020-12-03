6f37bf2f4b2b908e8c95dda2ba1ef2e43c0ecbee
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-03 10:06:37 +0000

    initial commit for contributor access

---
+++ src/main/java/com/codeclan/example/CourseBookingsJavaSpring/CourseBookingsJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.CourseBookingsJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBookingsJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBookingsJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/CourseBookingsJavaSpring/CourseBookingsJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.CourseBookingsJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingsJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
